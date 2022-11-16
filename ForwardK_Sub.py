fcb494b799da26f9782046b3d64e7988989f8a8e
m-wadzinski <79165989+m-wadzinski@users.noreply.github.com>
2022-11-16 16:29:53 -0500

    Update ForwardK_Sub.py

--- ForwardK_Sub.py
+++ ForwardK_Sub.py
@@ -2,33 +2,32 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray
 import numpy as np
-import sympy as sp
 
-class ForkinSubscriber(Node):
+class ForwardSubscriber(Node):
 
     def __init__(self):
-        super().__init__('forkin_subscriber')
-        self.subscription = self.create_subscription(Float32MultiArray, 'topic', self.listener_callback, 10)
+        super().__init__('forward_subscriber')
+        self.subscription = self.create_subscription(Float32MultiArray, 'inputQ', self.listener_callback, 10)
         self.subscription  # Recommended by tutorial to prevent error
 
     def listener_callback(self, msg):
-        l1,l2,l3 = sp.symbols("l1,l2,l3")
-        DH_L1 = (0, l1, msg.data[0] + np.pi/2 , -np.pi/2)   
-        DH_L2 = (l2, 0, msg.data[1], 0)
-        DH_L3 = (l3, 0, msg.data[2], 0)
-        A1 = dh2a(DH_L1)
-        A2 = dh2a(DH_L2)
-        A3 = dh2a(DH_L3)
-        self.get_logger().info("\nInput:\n %s \nTransformation:\n %s" % (msg.data, str(np.matmul(np.matmul(A1, A2), A3))))
+        DH = ([1.0, 1.0, msg.data[0], 0.0],
+              [1.0, 0.0, msg.data[1], 0.0],
+              [0.0, msg.data[2], 0.0, 0.0])
+        A = []
+        for x in DH:
+            a = dh2a(x)
+            A.append(a)
+        self.get_logger().info("\nInput:\n %s \nTransformation:\n %s" % (msg.data, str(np.matmul(np.matmul(A[0], A[1]), A[2]))))
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    forkin_subscriber = ForkinSubscriber()
+    forward_subscriber = ForwardSubscriber()
 
-    rclpy.spin(forkin_subscriber)
-    forkin_subscriber.destroy_node()
+    rclpy.spin(forward_subscriber)
+    forward_subscriber.destroy_node()
     rclpy.shutdown()
 
 
